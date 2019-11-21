d0d97c584c849f68322f12dcbada21b6dd90b652
Sywar <sywar.bennjima96@gmail.com>
2019-11-21 17:56:26 +0100

    demo commit

--- src/main/java/com/ditraacademy/travelagency/TravelagencyApplication.java
+++ src/main/java/com/ditraacademy/travelagency/TravelagencyApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TravelagencyApplication { //sjdklkhs
+public class TravelagencyApplication { //sjdklkhsjj
 
     public static void main(String[] args) {
         SpringApplication.run(TravelagencyApplication.class, args);
