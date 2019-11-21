4f0b258506168ad8b2c0970a28811a79dba0a822
Sywar <sywar.bennjima96@gmail.com>
2019-11-21 17:55:45 +0100

    Demo

--- src/main/java/com/ditraacademy/travelagency/TravelagencyApplication.java
+++ src/main/java/com/ditraacademy/travelagency/TravelagencyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TravelagencyApplication {
+public class TravelagencyApplication { //sjdklkhs
 
     public static void main(String[] args) {
         SpringApplication.run(TravelagencyApplication.class, args);
     }
-
+//demo
 }
